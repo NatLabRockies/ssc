2664ff1af940f043ec49ac9b140c2ca06f9a0aab
sjanzou <sjanzou@gmail.com>
2025-09-26 04:26:05 -0600

    Test running locally using std::sort instead of std::stable_sort
    For byLowestMarginalcost sorting only in disaptch_for_cost for AutoBTMTest_lib_battery_dispatch.TestSummerPeakGridChargingSubhourly test

--- shared/lib_battery_dispatch_automatic_btm.cpp
+++ shared/lib_battery_dispatch_automatic_btm.cpp
@@ -777,7 +777,8 @@ void dispatch_automatic_behind_the_meter_t::plan_dispatch_for_cost(dispatch_plan
     }
 
     // Iterating over sorted grid
-    std::stable_sort(sorted_grid.begin(), sorted_grid.end(), byLowestMarginalCost());
+//    std::stable_sort(sorted_grid.begin(), sorted_grid.end(), byLowestMarginalCost());
+    std::sort(sorted_grid.begin(), sorted_grid.end(), byLowestMarginalCost());
     // Find m hours to get required energy - hope we got today's energy yesterday (for morning peaks). Apportion between hrs of lowest marginal cost
     i = 0;
     while (requiredEnergy > 0 && i < _num_steps)

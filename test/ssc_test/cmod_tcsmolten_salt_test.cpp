7b92d3308d1b3d7e3131403ab5a42f5b66ff25cf
Brian Mirletz <brian.mirletz@nlr.gov>
2026-03-27 08:58:49 -0600

    Add recommended include from compiler failure

--- test/ssc_test/cmod_tcsmolten_salt_test.cpp
+++ test/ssc_test/cmod_tcsmolten_salt_test.cpp
@@ -35,6 +35,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "tcsmolten_salt_defaults.h"
 #include "cmod_tcsmolten_salt_test.h"
 #include "csp_common_test.h"
+#include <iomanip>
+
 
 namespace csp_tower {}
 using namespace csp_tower;

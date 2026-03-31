10a49a62b118f4dcabc965418c57bb311ddf7f45
sjanzou <sjanzou@gmail.com>
2026-03-31 05:28:27 -0400

    Update failing to compile on RHEL8  tests

--- test/ssc_test/cmod_tcsmolten_salt_test.cpp
+++ test/ssc_test/cmod_tcsmolten_salt_test.cpp
@@ -30,7 +30,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
+#include <iomanip>
 #include <gtest/gtest.h>
 #include "tcsmolten_salt_defaults.h"
 #include "cmod_tcsmolten_salt_test.h"
--- test/ssc_test/cmod_trough_physical_test.cpp
+++ test/ssc_test/cmod_trough_physical_test.cpp
@@ -30,7 +30,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
+#include <iomanip>
 #include <gtest/gtest.h>
 #include "trough_physical_defaults.h"
 #include "csp_common_test.h"

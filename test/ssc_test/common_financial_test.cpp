d5c4ccbb17f0d582150b7789bfeddde338877c91
Brian Mirletz <brian.mirletz@nlr.gov>
2026-04-24 15:40:10 -0600

    Add test for non-depreciable assets

--- test/ssc_test/common_financial_test.cpp
+++ test/ssc_test/common_financial_test.cpp
@@ -182,3 +182,42 @@ TEST_F(common_financial_test, test_calculated_matrix_all_macrs) {
     EXPECT_DOUBLE_EQ(itc_depr.itc_disallow_fed_percent_macrs_5, installed_cost * 0.3 * 0.5 * 0.5);
     EXPECT_DOUBLE_EQ(itc_depr.get_itc_fed(1), installed_cost * 0.3 * 0.5);
 }
+
+TEST_F(common_financial_test, test_calculated_matrix_non_depreciable) {
+
+    //col 0=tech no, col 1=basis amount ($), col 2=% of total installed cost, col 3=fed itc qual 0/1, col 4=state itc qual 0/1, col 5=depreciation selection(0=macrs 5, 1=macrs 15, 2=sl 5, 3=sl 15, 4=sl 20, 5=sl 39, 6=custom)
+    ssc_number_t p_depr_basis_mat[24] = { 1, 1000000, 50, 1, 1, 0,
+    2, 1000000, 50, 0, 0, 7 };
+    cm.assign("depr_basis_mat", var_data(p_depr_basis_mat, 2, 6));
+    cm.assign("depr_en_basis_mat", var_data(1));
+
+    ssc_number_t itc_fed_percent[] = { 30.0f };
+    cm.assign("itc_fed_percent", var_data(itc_fed_percent, 1));
+
+    itc_and_depreciation_calculations itc_depr(&cm, 25);
+    itc_depr.depr_fed_reduction = 0;
+    itc_depr.depr_sta_reduction = 0;
+
+    // Test reallocation to depreciable basis
+    EXPECT_NEAR(itc_depr.depr_alloc_macrs_5_frac
+        + itc_depr.depr_alloc_macrs_15_frac
+        + itc_depr.depr_alloc_sl_5_frac
+        + itc_depr.depr_alloc_sl_15_frac
+        + itc_depr.depr_alloc_sl_20_frac
+        + itc_depr.depr_alloc_sl_39_frac
+        + itc_depr.depr_alloc_custom_frac,
+        1.0, 1e-9);
+
+    // Example installed cost from PVWatts Single Owner scenario: $4,000,000
+    const double installed_cost = 2'000'000.0;
+    itc_depr.calc_basis(installed_cost, installed_cost);
+
+    // Only half of assets are depreciable for this test
+    EXPECT_NEAR(itc_depr.depr_alloc_total, installed_cost * 0.5, 1.0);
+
+    EXPECT_DOUBLE_EQ(itc_depr.itc_sta_per, 0.0);
+    EXPECT_DOUBLE_EQ(itc_depr.itc_fed_per, installed_cost * 0.3 * 0.5);
+    EXPECT_DOUBLE_EQ(itc_depr.itc_fed_qual_total, installed_cost * 0.5);
+    EXPECT_DOUBLE_EQ(itc_depr.itc_disallow_fed_percent_macrs_5, installed_cost * 0.3 * 0.5 * 0.5);
+    EXPECT_DOUBLE_EQ(itc_depr.get_itc_fed(1), installed_cost * 0.3 * 0.5);
+}
